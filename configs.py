cc80799b4c57a4e34fbefe35d6973c57d48d7fa5
B0bby D1g1tal <B0bbyD1g1tal@protonmail.com>
2021-02-25 18:27:44 +0200

    Platform.IO

--- configs.py
+++ configs.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+import os
 import pathlib
 import shutil
 import subprocess
@@ -7,9 +10,15 @@ import subprocess
 # Set ENV variables
 # =============================================================================
 ###############################################################################
+PIO_BOARD_ENV = os.environ["PIO_BOARD_ENV"]
+PIO_DIR = os.environ["PIO_DIR"]
 
-PIO_DIR = "/home/b0bby/code/Marlin-Firmware-Builder/"  # or $(pwd)
-GIT_BRANCH = "bugfix-2.0.x"
+GIT_BRANCH = os.environ["GIT_BRANCH"]
+
+# PIO_DIR="/home/b0bby/code/Marlin-Firmware-Builder/"
+# FIRMWARE_BIN_DIR="/firmware/"
+# GIT_BRANCH="bugfix-2.0.x"
+# PIO_BOARD_ENV="STM32F103RET6_creality"
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # Names should be the same as in the Configurations repository path:
 # https://github.com/MarlinFirmware/Configurations/tree/$GIT_BRANCH/config/examples/$MANUFACTURER/$MODEL/$BOARD/*.h
@@ -17,8 +26,6 @@ GIT_BRANCH = "bugfix-2.0.x"
 MANUFACTURER = "Creality"
 MODEL = "Ender-3 Pro"
 BOARD = "CrealityV427"
-
-PIO_BOARD_ENV = "STM32F103RET6_creality"
 ###############################################################################
 # ==============================================================================
 # Set script variables
@@ -27,18 +34,15 @@ PIO_BOARD_ENV = "STM32F103RET6_creality"
 AUTHOR = "B0bby D1g1tal"
 # sed separator set to ^
 SED = "^"
-
 ###############################################################################
 # Marlin Github repositories and branch
 ###############################################################################
 MARLIN_GITHUB_URL = "https://github.com/MarlinFirmware/"
 MARLIN_FW_REPO = "Marlin"
 MARLIN_CONFIG_REPO = "Configurations"
-
 ###############################################################################
 # Marlin Firmware and Configurations local paths
 ###############################################################################
-
 CONFIGS_PATH = pathlib.Path(f"{PIO_DIR}{MARLIN_CONFIG_REPO}/config/examples/{MANUFACTURER}/{MODEL}/{BOARD}/")
 MARLIN_PIO_PATH = pathlib.Path(f"{PIO_DIR}{MARLIN_FW_REPO}/Marlin/")
 ###############################################################################
@@ -66,50 +70,50 @@ shutil.copytree(CONFIGS_PATH,
 # platformio.ini
 ###############################################################################
 DEFAULT_ENVS = "default_envs = "
-subprocess.run(["sed", "- i", "- e",
-                f"s{SED}{DEFAULT_ENVS}.*{SED}{DEFAULT_ENVS}{PIO_BOARD_ENV}{SED}"
+subprocess.run(["sed", "-i", "-e",
+                f"s{SED}{DEFAULT_ENVS}.*{SED}{DEFAULT_ENVS}{PIO_BOARD_ENV}{SED}",
                 f"{PIO_DIR}{MARLIN_FW_REPO}/platformio.ini"])
 
 ###############################################################################
 # Configuration.h
 ###############################################################################
 STRING_CONFIG_H_AUTHOR = "#define STRING_CONFIG_H_AUTHOR"
-subprocess.run(["sed", "- i", "- e",
-                f"s{SED}{STRING_CONFIG_H_AUTHOR} \"(.*,{SED}{STRING_CONFIG_H_AUTHOR} \"({AUTHOR},{SED}"
+subprocess.run(["sed", "-i", "-e",
+                f"s{SED}{STRING_CONFIG_H_AUTHOR} \"(.*,{SED}{STRING_CONFIG_H_AUTHOR} \"({AUTHOR},{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
 SERIAL_PORT_2 = "#define SERIAL_PORT_2"
-subprocess.run(["sed", "- i", "- e",
-                f"s//{SED}{SERIAL_PORT_2}.*{SED}{SERIAL_PORT_2} 3{SED}",
+subprocess.run(["sed", "-i", "-e",
+                f"s{SED}//{SERIAL_PORT_2} .*{SED}{SERIAL_PORT_2} 3{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
 BLTOUCH = "#define BLTOUCH"
-subprocess.run(["sed", "- i", "- e",
+subprocess.run(["sed", "-i", "-e",
                 f"s{SED}//{BLTOUCH}{SED}{BLTOUCH}{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
 AUTO_BED_LEVELING_BILINEAR = "#define AUTO_BED_LEVELING_BILINEAR"
-subprocess.run(["sed", "- i", "- e",
+subprocess.run(["sed", "-i", "-e",
                 f"s{SED}//{AUTO_BED_LEVELING_BILINEAR}{SED}{AUTO_BED_LEVELING_BILINEAR}{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
 Z_SAFE_HOMING = "#define Z_SAFE_HOMING"
-subprocess.run(["sed", "- i", "- e",
+subprocess.run(["sed", "-i", "-e",
                 f"s{SED}//{Z_SAFE_HOMING}{SED}{Z_SAFE_HOMING}{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
-NOZZLE_TO_PROBE_OFFSET = "#define NOZZLE_TO_PROBE_OFFSET"
-subprocess.run(["sed", "- i", "- e",
-                f"s{SED}{NOZZLE_TO_PROBE_OFFSET} {10, 10, 0}{SED}{NOZZLE_TO_PROBE_OFFSET} {-42, -10, 0}{SED}",
-                f"{MARLIN_PIO_PATH}/Configuration.h"])
+# NOZZLE_TO_PROBE_OFFSET = "#define NOZZLE_TO_PROBE_OFFSET"
+# subprocess.run(["sed", "-i", "-e",
+#                 f"s{SED}{NOZZLE_TO_PROBE_OFFSET}.*{SED}{NOZZLE_TO_PROBE_OFFSET} {-42, -10, 0}{SED}",
+#                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
 GRID_MAX_POINTS_X = "#define GRID_MAX_POINTS_X"
-subprocess.run(["sed", "- i", "- e",
-                f"s{SED}{GRID_MAX_POINTS_X} .{SED}{GRID_MAX_POINTS_X} 5{SED}",
+subprocess.run(["sed", "-i", "-e",
+                f"s{SED}{GRID_MAX_POINTS_X}.*{SED}{GRID_MAX_POINTS_X} 5{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
 XY_PROBE_SPEED = "#define XY_PROBE_SPEED"
-subprocess.run(["sed", "- i", "- e",
+subprocess.run(["sed", "-i", "-e",
                 f"s{SED}{XY_PROBE_SPEED} .*{SED}{XY_PROBE_SPEED} (150*60){SED}",
                 f"{MARLIN_PIO_PATH}/Configuration.h"])
 
@@ -117,17 +121,17 @@ subprocess.run(["sed", "- i", "- e",
 # Configuration_adv.h
 ###############################################################################
 PROBE_OFFSET_WIZARD = "#define PROBE_OFFSET_WIZARD"
-subprocess.run(["sed", "- i", "- e",
+subprocess.run(["sed", "-i", "-e",
                 f"s{SED}//{PROBE_OFFSET_WIZARD}{SED}{PROBE_OFFSET_WIZARD}{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration_adv.h"])
 
 PROBE_OFFSET_WIZARD_START_Z = "#define PROBE_OFFSET_WIZARD_START_Z"
-subprocess.run(["sed", "- i", "- e",
+subprocess.run(["sed", "-i", "-e",
                 f"s{SED}//{PROBE_OFFSET_WIZARD_START_Z}{SED}{PROBE_OFFSET_WIZARD_START_Z}{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration_adv.h"])
 
 BABYSTEP_ZPROBE_OFFSET = "#define BABYSTEP_ZPROBE_OFFSET"
-subprocess.run(["sed", "- i", "- e",
+subprocess.run(["sed", "-i", "-e",
                 f"s{SED}//{BABYSTEP_ZPROBE_OFFSET}{SED}{BABYSTEP_ZPROBE_OFFSET}{SED}",
                 f"{MARLIN_PIO_PATH}/Configuration_adv.h"])
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+cd "${PIO_DIR}/Marlin/" && \
 platformio run -e "${PIO_BOARD_ENV}" && \
 cp "${PIO_DIR}/Marlin/.pio/build/${PIO_BOARD_ENV}/firmware-*.bin" "${FIRMWARE_BIN_DIR}"
