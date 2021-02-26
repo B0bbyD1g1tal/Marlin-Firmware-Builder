c4514c8235d145ed698c60d26e836ed51d8f0135
B0bby D1g1tal <B0bbyD1g1tal@protonmail.com>
2021-02-26 22:03:32 +0200

    ShellCheck

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-FIRMWARE_BIN="${WORK_DIR}/Marlin/.pio/build/${PIO_BOARD_ENV}/firmware-*bin"
-
 cd "${WORK_DIR}"/Marlin/ || exit &&
   pio run -e "${PIO_BOARD_ENV}" &&
-  cp "${FIRMWARE_BIN}" "${FIRMWARE_BIN_DIR}"
+  cp "${WORK_DIR}"/Marlin/.pio/build/"${PIO_BOARD_ENV}"/firmware-*bin \
+    "${FIRMWARE_BIN_DIR}"
