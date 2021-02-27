c9f52a0537a90a9d5c2cef719afcc579321deafc
B0bby D1g1tal <B0bbyD1g1tal@protonmail.com>
2021-02-27 16:06:36 +0200

    TODOs

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,9 +3,30 @@
 set -e
 set -x
 ###############################################################################
+# Build and copy firmware-*.bin
+###############################################################################
+BINARY_FILE_NAME=\
+$(echo "$MODEL" | tr -d " ")-"${BOARD}"-"${GIT_BRANCH}"-$(date +"%d-%b-%y").bin
+
 cd "${WORK_DIR}"/Marlin/ || exit &&
   pio run -e "${PIO_BOARD}" &&
   cp "${WORK_DIR}"/Marlin/.pio/build/"${PIO_BOARD}"/firmware-*bin \
-    "${FIRMWARE_BIN_DIR}"
+    "${FIRMWARE_BIN_DIR}"/"${BINARY_FILE_NAME}"
 ###############################################################################
 # TODO Branch &| PIO Board &| Printer selection to be done here
+
+# TODO -check-branches-and- just checkout the selected
+
+# TODO move to entrypoint
+# Copy selected printer configs to PIO project
+#copytree(MARLIN_PRINTER_CONFIG, PIO_CONFIGS,
+#         dirs_exist_ok=True)
+
+# TODO move to entrypoint
+# Set Default ENV in platformio.ini
+#DEFAULT_PIO_ENV = 'default_envs = '
+#run(['sed', '-i', '-e',
+#     f's^{DEFAULT_PIO_ENV}.*^{DEFAULT_PIO_ENV}{environ["PIO_BOARD"]}^',
+#     f'{PIO_PROJECT}/platformio.ini'],
+#    check=True)
+# TODO move to entrypoint
