41c01aa52d7590e8a2a850aa21eaf33b21efef81
B0bby D1g1tal <B0bbyD1g1tal@protonmail.com>
2021-02-28 02:19:22 +0200

    linters ...

--- scripts/build_bootstrapper.py
+++ scripts/build_bootstrapper.py
@@ -12,7 +12,7 @@ from os import environ, chdir
 from shutil import copytree
 from subprocess import run
 from pathlib import Path
-
+# pylint: disable=W0511
 ###############################################################################
 # Marlin
 ###############################################################################
@@ -34,8 +34,8 @@ chdir(Path(environ["WORK_DIR"]))
 run(git_firmware, check=True)
 run(git_configs, check=True)
 # Add the specified 3D-Printer config in PIO project, if all ENVs are available
-# TODO images must be tagged with printer's name if used # pylint: disable=fixme
-# TODO inspect case of 2 different printers on build and runtime # pylint: disable=fixme
+# TODO images must be tagged with printer's name if used
+# TODO inspect case of 2 different printers on build and runtime
 if environ["MANUFACTURER"] and environ["MODEL"] and environ["BOARD"]:
     MARLIN_PRINTER_CONFIG = Path(
         f'{environ["WORK_DIR"]}{MARLIN_CONFIG_REPO}/config/examples/'
