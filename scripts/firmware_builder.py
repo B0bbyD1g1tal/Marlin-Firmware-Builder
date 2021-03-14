097e6f279f9db2103902b86942093ded6410ff7c
B0bby D1g1tal <B0bbyD1g1tal@protonmail.com>
2021-03-14 11:46:02 +0200

    firmware folder structure builder.env ENVs info added

--- scripts/firmware_builder.py
+++ scripts/firmware_builder.py
@@ -6,7 +6,7 @@ sets environment for the specified Model/Board
 """
 
 from datetime import datetime
-from os import environ, chdir
+from os import environ, chdir, makedirs
 from subprocess import run
 from pathlib import Path
 from shutil import copytree, copyfile
@@ -16,6 +16,7 @@ def extract_values(file, value, token, redundant):
     """
     Extracts values from files
     and removes redundant parts of them
+    token ensures that the correct line is matched
     """
     with open(file, 'r') as text:
         return [line.strip().split()[-1].replace(redundant, '')
@@ -71,11 +72,18 @@ run(['pio', 'run', '-e', f'{PIO_ENV}'], check=True)
 COMPILED_FW = \
     sorted(Path(f'{MARLIN_FW}/.pio/build/{PIO_ENV}/').glob('firmware*'))
 FW_FILE_NAME = \
-    f'{environ["MODEL"].replace(" ", "")}+{MOTHERBOARD}.' \
-    f'{BRANCH}_{datetime.now().strftime("%d-%b-%y_%H%M%S")}'
+    f'{environ["MODEL"].replace(" ", "")}-{MOTHERBOARD}.' \
+    f'{BRANCH}-{datetime.now().strftime("%d-%b-%y_%H%M%S")}'
+
+makedirs(f'{environ["FIRMWARE_BIN_DIR"]}/'
+         f'{environ["MANUFACTURER"]}/{environ["MODEL"]}/',
+         exist_ok=True)
 
 for firmware in COMPILED_FW:
+    fw_extension = str(firmware).split('.')[-1]
     copyfile(firmware,
              Path(f'{environ["FIRMWARE_BIN_DIR"]}/'
                   f'{environ["MANUFACTURER"]}/{environ["MODEL"]}/'
-                  f'{FW_FILE_NAME}'))
+                  f'{FW_FILE_NAME}.{fw_extension}'))
+
+###############################################################################
